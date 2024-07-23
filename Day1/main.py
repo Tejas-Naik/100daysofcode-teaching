a8e30075208803508d91df4a4bb50f8f25aa995f
Tejas-Naik <rntejas2005@gmail.com>
2024-07-23 15:37:29 +0530

    Day1

---
+++ Day1/main.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+print(100)
+print(True)
+print("Hello, Tejas!")
+
+name = input("What is your name? ")
+print("Hello", name)
+
+# Band name generator
+# CIty & Pet name
+# City+PetName
+
